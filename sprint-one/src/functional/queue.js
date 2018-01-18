b537a242a73758319c0b33ea94a612975cf4fe9b
scottbarclay <scottdbarclay@gmail.com>
2018-01-17 16:32:57 -0800

    completed queue and stack of functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
-
+  var count = 0;
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0 ) {
+      // identify first key in storage
+      var first = storage[0];
+      //and delete it
+      delete storage[0];
+
+      for (var key in storage) {
+        var val = storage[key];
+        storage[+key - 1] = val;
+          
+      }
+      // decrease count
+      count--;
+      return first;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
