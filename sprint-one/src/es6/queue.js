a9332c1942ec4d5487cba2b07f7f26c6f1646f3d
scottbarclay <scottdbarclay@gmail.com>
2018-01-18 10:51:55 -0800

    completed first Sprint

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,44 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Declare constructor function and add properties
   constructor() {
+    // Set up variable to keep track of number of values in queue
+    this.count = 0;
+    // Property for saving holding values in queue
+    this.storage = {}; 
   }
+  
+  // Method which adds value to queue; takes value as parameter
+  enqueue(value) {
+    // add value at next key of storage based on count
+    this.storage[this.count] = value;
+    // increment size of queue (count)
+    this.count++;
+  }
+  
+  // method which returns and deletes a value from front of queue
+  dequeue() {
+    // verify queue is not empty
+    if (this.count > 0) {
+      // capture the value of the first queue item 
+      var first = this.storage[0];
+      // delete the first item (FIFO)
+      delete this.storage[0];
+      // decrease size of queue
+      this.count--;
+      // move to all item to their new position in queue
+      for (var key in this.storage) {
+        var temp = this.storage[key];
+        this.storage[+key - 1] = temp;
+      }
+      return first;
+    }
+   
+
+
+  }
+  
+  size() {
+    return this.count;
+  } 
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {}; 
   }
-
+  
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  
+  pop() {
+    if (this.count > 0) {
+      var last = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count--;
+      return last;
+    }
+  }
+  
+  size() {
+    return this.count;
+  }
+ 
 }
