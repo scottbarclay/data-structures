6ace89f1f4efe542ee0384544bcfa3e46d83955b
scottbarclay <scottdbarclay@gmail.com>
2018-01-18 18:51:45 -0800

    struggling with contains of linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,69 @@
+
+// Specificatipon
+  // No inputs; output is a linked list, which is an object (that includes other objects
+  // in series. No side effects.
+// Justification
+  // the function creates a series of linked objects
+// Explanation
+  // function will instantiate a linked list, including properties
+  // and methods
+
 var LinkedList = function() {
+  // start with an empty list object to return
   var list = {};
+  // assign properties to the object
   list.head = null;
   list.tail = null;
 
+  // assign methods to object
+
+  // add the new last object to an existing list
   list.addToTail = function(value) {
+    //debugger;
+    var newNode = Node(value);
+    if (list.head !== null) {
+      var currentTail = list.tail;
+      list.tail = newNode;
+      currentTail.next = newNode;      
+    } else {
+      list.head = newNode;
+      list.tail = newNode;
+    } 
+    // verify if list is empty
   };
 
+  // remove the first object and re-establish llnked list heirarchy and links
   list.removeHead = function() {
+    if (list.head !== null) {
+      var nodeToRemove = list.head.value;
+      list.head = list.head.next;
+      return nodeToRemove;   
+    }  
   };
 
+  // verify that an object exists in the list
   list.contains = function(target) {
+    if (list.head === null) {
+      return false;
+    } else {
+      var listNode = list.head;
+      var testNode = function(listNode) {
+        if(listNode.value === target){
+          return true;
+        }
+        //if(listNode === list.tail && listNode.value !== target){
+        if(listNode === undefined) {
+          return false;
+        }
+        testNode(listNode.next);
+      };
+      testNode(listNode);
+    }
   };
-
   return list;
 };
 
+// creates a new node to add to the list
 var Node = function(value) {
   var node = {};
 
