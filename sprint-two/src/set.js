92f486cd97926da3df5951f972e4bebba4582887
scottbarclay <scottdbarclay@gmail.com>
2018-01-19 18:46:00 -0800

    add set solution

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) {  
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
+                                                                                                                                                                                                                                                                                                                                                                           
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
